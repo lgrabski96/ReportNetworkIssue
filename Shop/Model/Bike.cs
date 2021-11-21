216fe25a58ded0c063ca05546e28cab2e961cd5a
Piotr <piotr.spikowski@hotmail.com>
2021-11-21 20:32:44 +0100

    Sample

---
+++ Shop/Model/Bike.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Model
+{
+    public class Bike : Vehicle
+    {
+        public override decimal CalculateCost(int kmRange) // => 0; (the same as below)
+        {
+            return 0;
+        }
+    }
+}
