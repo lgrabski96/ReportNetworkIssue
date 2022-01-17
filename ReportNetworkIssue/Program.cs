4b534a86d29d235e1cbe81fc46604cdf1addd156
Lukasz Grabski <lukasz.grabski@capgemini.com>
2022-01-17 10:48:18 +0100

    Rename and project cleaning

--- ReportNetworkIssue/Program.cs
+++ ReportNetworkIssue/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Office.Interop.Outlook;
 using System;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace ReportNetworkIssue
 {
@@ -10,16 +11,22 @@ namespace ReportNetworkIssue
         {
             var outlook = new Application();
 
+            string message = "Message Send";
+
             try
             {
                 MailItem mail = outlook.CreateItem(OlItemType.olMailItem) as MailItem;
                 mail.Subject = "Netwotk Issue";
                 mail.Body = "A problem has been detected";
-                mail.To = "marek.romanowicz@capgemini.com";
+                mail.To = "lukasz.grabski@capgemini.com";
                 // mail.Display();
                 mail.Send();
+
+                MessageBox.Show(message);
+
                 Console.WriteLine("Email was send to ithelpdeskglobal ");
                 Thread.Sleep(5000);
+
 
                 /*
                 // TODO: Optionally we can resolve addresses from Exchange Server if that one is used
