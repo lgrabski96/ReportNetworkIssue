2370656b234b504e9911b59df828fd30b5358a3b
Lukasz Grabski <lukasz.grabski@capgemini.com>
2022-01-20 11:43:16 +0100

    Aded System windows forms

--- ReportNetworkIssue/SendEmail.ps1
+++ ReportNetworkIssue/SendEmail.ps1
@@ -1,4 +1,7 @@
-﻿try
+﻿Add-Type -AssemblyName System.Windows.Forms
+Write-Host "Capgemini - Network Reporting tool"
+Write-Host "Please wait for the network report to generate..."
+try
 {
     $DesktopPath = [System.Environment]::GetFolderPath("Desktop")
     $env:computername | Out-File -FilePath $DesktopPath\NetworkandComputerdetails.txt
@@ -12,19 +15,34 @@
 
     $mail.To = "ithelp.global@capgemini.com"
     $mail.Subject = "Network Issue"
-    $mail.Body = "Hello. I had an issue with network connection that occured around $date. Please check the possible reason and help to solve it. Thank You."
+    $mail.Body = "Hello. I had an issue with network connection that occurred around $date. 
+    Please check the possible reason and help to solve it. 
+    This email was generated automatically and contains an attachment file containing basic network connection and computer report.
+    Thank You."
 
     $attachment1 = "$DesktopPath\NetworkandComputerdetails.txt"
     $mail.Attachments.add($attachment1)
 
     $mail.Send()
 
-    [System.Windows.MessageBox]::Show('Email has been successfully sent')
+    $ButtonType = [System.Windows.Forms.MessageBoxButtons]::OK
+    $MessageIcon = [System.Windows.Forms.MessageBoxIcon]::Information
+    $MessageBody = "Network report has been successfully sent to ithelp.global@capgemini.com"
+    $MessageTitle = "Message successfully sent"
+    [System.Windows.Forms.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+    Start-Sleep -Seconds 1
 
 }
-catch
+catch [Exception]
 {
-    [System.Windows.MessageBox]::Show('Error: Unable to send an email')
-
-   
+    Write-Host "An exception has accrued:"
+    Write-Host $_.Exception.Message
+    
+    $ButtonType = [System.Windows.Forms.MessageBoxButtons]::OK
+    $MessageIcon = [System.Windows.Forms.MessageBoxIcon]::Stop
+    $MessageBody = "Error: Unable to send an email.`rCheck internet connection and try again."
+    $MessageTitle = "Error while sending an email"
+                     
+    [System.Windows.Forms.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+    Start-Sleep -Seconds 1
 }
